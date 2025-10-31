5e2d4287b4a6e814b8ba61dbdd12fdfebc8c656a
Qaasim-md <qaasimdavids1@gmail.com>
2025-10-31 08:50:14 +0200

    add exercise-2

---
+++ exercise 2.js
@@ -0,0 +1,12 @@
+let number1 = 10;
+let number2 = 20;
+
+let numberString = number1.toString();
+console.log(numberString);
+
+let sum = number1 + number2;
+console.log(sum);
+
+let product = number1 * number2;
+console.log(product);
+
